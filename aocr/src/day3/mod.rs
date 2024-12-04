f16991937336fa2020b55a17ce7ff59bc29cf7c3
amarrerod <alemarrerodiaz@gmail.com>
2024-12-04 13:32:07 +0000

    🚧 Working on error handling

--- aocr/src/day3/mod.rs
+++ aocr/src/day3/mod.rs
@@ -12,38 +12,43 @@ fn read_file(filename: &str) -> io::Result<String> {
     Ok(content)
 }
 
-fn solve(content: &str) -> i32 {
+fn solve(content: &str) -> Result<i32, i32> {
     let re = Regex::new(r"mul\(\d+,\d+\)|do\(+\)|don\'t\(+\)").unwrap();
     let instructions: Vec<&str> = re.find_iter(content).map(|cap| cap.as_str()).collect();
     let mut compute: bool = true;
-    let total_sum: i32 = instructions
+    let total_sum = instructions
         .into_iter()
         .map(|i| {
             if i == "don't()" {
                 compute = false;
-                0
+                Ok(0)
             } else if i == "do()" {
                 compute = true;
-                0
+                Ok(0)
             } else if compute {
-                i[4..=(i.find(")").unwrap() - 1)]
+                Ok(i[4..=(i.find(")").ok_or(-1)? - 1)]
                     .split(",")
-                    .map(|x| x.parse::<i32>().unwrap_or(0))
-                    .fold(1, |acc, item| acc * item)
+                    .map(|x| x.parse::<i32>().map_err(|_| -2))
+                    .fold(1, |acc, item| acc * item.unwrap()))
             } else {
-                0
+                Ok(0)
             }
         })
-        .sum();
+        .collect::<Result<Vec<i32>, i32>>()
+        .map(|results| results.into_iter().sum());
 
-    println!("The total sum is: {:#?}", total_sum);
+    println!("The total sum is: {:#?}", total_sum.unwrap());
     total_sum
 }
 
 pub fn solve_parts() {
     println!("{:} Day 3 {:}", "=".repeat(20), "=".repeat(20));
 
-    let content = read_file("input.txt").unwrap_or(String::new());
+    let content = read_file("test2.txt").unwrap_or(String::new());
     println!("{:}", content);
-    solve(&content);
+    let r = solve(&content);
+    match r {
+        Err(_e) => println!("Error in the solution"),
+        Ok(_) => {}
+    }
 }
