68e8a72199e1384a6d2504662cfb137091b5bb50
amarrerod <alemarrerodiaz@gmail.com>
2024-12-14 15:07:33 +0000

    ⚡️ Improves performance

--- python/day14/day14.py
+++ python/day14/day14.py
@@ -12,80 +12,61 @@
 
 from pathlib import Path
 import re
-from tqdm import tqdm
-from collections import Counter
 from functools import reduce
 from operator import mul
 
 
 def read_robots(filename: str = "test.txt"):
     raw = Path(__file__).with_name(filename).read_text().splitlines()
-    positions: list[tuple[int]] = []
-    speeds: list[tuple[int]] = []
+    robots = []
     for line in raw:
         (x, y, vx, vy) = list(map(int, re.findall(r"-?\d+", line)))
-        positions.append((x, y))
-        speeds.append((vx, vy))
+        robots.append((x, y, vx, vy))
 
-    return positions, speeds
+    return robots
 
 
-def print_robots(robots, x_limit: int = 11, y_limit: int = 7):
-    s = ""
-    for y in range(y_limit):
-        for x in range(x_limit):
+def print_robots(robots, width: int = 11, height: int = 7):
+    s = "=" * 40 + "\n"
+    for y in range(height):
+        for x in range(width):
             s += "🤖" if (x, y) in robots else "."
         s += "\n"
-
+    s += "=" * 40 + "\n"
     print(s, end="\r")
-
-
-def count_by_quadrant(robots, quadrant):
-    x0, x1, y0, y1 = quadrant
-    s = "\n"
-    counter = 0
-    for x in range(x0, x1 + 1):
-        for y in range(y0, y1 + 1):
-            if (y, x) in robots:
-                s += str(robots[(y, x)])
-                counter += robots[(y, x)]
-            else:
-                s += "."
-        s += "\n"
-
-    print(s, end="\r")
-    return counter
 
 
 def move_robots(
     robots: list[int],
-    speeds: list[int],
-    x_limit: int = 11,
-    y_limit: int = 7,
+    width: int = 11,
+    height: int = 7,
     seconds: int = 100,
 ):
-    for _ in tqdm(range(seconds)):
-        for i in range(len(robots)):
-            (x, y), (vx, vy) = robots[i], speeds[i]
-            nx = (x + vx) % x_limit
-            ny = (y + vy) % y_limit
-            robots[i] = (nx, ny)
+    quadrants = [0, 0, 0, 0]
+    final_positions = []
+    width_limit, height_limit = width // 2, height // 2
 
-    quad_x_lim, quad_y_lim = (x_limit // 2) - 1, (y_limit // 2) - 1
-    first = (0, quad_y_lim, 0, quad_x_lim)
-    second = (0, quad_y_lim, quad_x_lim + 2, x_limit - 1)
-    third = (quad_y_lim + 2, y_limit - 1, 0, quad_x_lim)
-    fourth = (quad_y_lim + 2, y_limit - 1, quad_x_lim + 2, x_limit - 1)
+    for x, y, vx, vy in robots:
+        nx = (vx * seconds + x) % width
+        ny = (vy * seconds + y) % height
+        final_positions.append((nx, ny))
 
-    robot_counter = Counter(robots)
-    quadrants = [
-        count_by_quadrant(robot_counter, q) for q in (first, second, third, fourth)
-    ]
+        if nx < width_limit and ny < height_limit:
+            quadrants[0] += 1
+        elif nx > width_limit and ny < height_limit:
+            quadrants[1] += 1
+        elif nx < width_limit and ny > height_limit:
+            quadrants[2] += 1
+        elif nx > width_limit and ny > height_limit:
+            quadrants[3] += 1
+
+    print_robots(final_positions)
     return reduce(mul, [s if s != 0 else 1 for s in quadrants], 1)
 
 
 if __name__ == "__main__":
-    robots, speeds = read_robots("input.txt")
-    print_robots(robots)
-    factor = move_robots(robots, speeds, x_limit=101, y_limit=103)
+    robots = read_robots("test.txt")
+    positions = [(x, y) for (x, y, _, _) in robots]
+    print_robots(positions)
+    factor = move_robots(robots, width=11, height=7)
     print(factor)
